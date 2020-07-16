c439440972965e1f068fc9b86bd3a105fada0196
Jordan <jordan.ziskin@yahoo.com>
2020-07-16 01:22:07 -0400

    Wrote two functions to cache matrix inversions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix functions creates an S3 object that can be used in the 
+## cacheSolve function which finds and stores the inverse of the matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix creates an object to be used in cacheSolve to store inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y){
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) i <<- inverse
+    getinv <- function() i    
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
+
+## takes makeCacheMatrix object, returns and stores inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinv()
+    if(!is.null(i)){
+        message('getting cached data')
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinv(i)
+    i
 }
+
+## Testing
+# m <- matrix(c(3, -1, 2, 5, 1, 0, -2, 3, 4),nrow = 3, byrow = TRUE)
+# a <- makeCacheMatrix(m)
+# cacheSolve(a)
+# cacheSolve(a)
